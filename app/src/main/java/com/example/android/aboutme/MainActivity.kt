a6a0c8ff7cb9fef34cbe85b5ada1811d1a84ca38
Danny Loh <dannylohhy1998@hotmail.com>
2020-07-03 00:28:21 +0800

    codelab 2.4 commit by lohhy

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /*setContentView(R.layout.activity_main)*/
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.myName = myName
